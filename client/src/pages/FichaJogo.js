a59733f995bc78c8c707474c246dc110842c18e0
filipe-21906899 <61635307+filipe-21906899@users.noreply.github.com>
2023-08-17 01:53:22 +0100

    Update
    Ficha Equipa Jogadores e Tecnicos a mostrar falta só a outra tabela

--- client/src/pages/FichaJogo.js
+++ client/src/pages/FichaJogo.js
@@ -14,10 +14,62 @@ function FichaJogo() {
   const [clubeOptions, setClubeOptions] = useState([]);
   const [escalaoOptions2, setEscalaoOptions2] = useState([]);
   const [clubes, setClubes] = useState([]);
+  const [showAlert, setShowAlert] = useState(false)
+  const [detailedJogadores, setDetailedJogadores] = useState([]);
+  const [detailedTecnico, setDetailedTecnico] = useState([]);
+  const [formSubmitted, setFormSubmitted] = useState(false);
+  const [tecnicosType, setTecnicosType] = useState([]);
 
-  const initialValues = {};
 
-  const validationSchema = Yup.object().shape()
+  const initialValues = {
+    EscalaoId: "",
+    Home: "",
+    Away: "",
+  };
+
+  const validationSchema = Yup.object().shape(
+    {
+      EscalaoId: Yup.string()
+        .required("Campo Obrigatório"),
+
+      Home: Yup.string()
+        .required("Campo Obrigatório"),
+
+      Away: Yup.string()
+        .required("Campo Obrigatório"),
+    }
+  )
+
+  const initialValues1 = {
+    EscalaoId: "",
+    ClubeId: "",
+  };
+
+  const validationSchema1 = Yup.object().shape(
+    {
+      ClubeId: Yup.string()
+        .required("Campo Obrigatório"),
+
+      EscalaoId: Yup.string()
+        .required("Campo Obrigatório"),
+    }
+  )
+
+  useEffect(() => {
+    const fetchOptions = async () => {
+      try {
+        const tecnicosTypeResponse = await fetch('http://localhost:3001/tecnicos_type');
+        const tecnicosTypeData = await tecnicosTypeResponse.json();
+
+        setTecnicosType(tecnicosTypeData);
+
+      } catch (error) {
+        console.error('Error fetching options:', error);
+      }
+    };
+
+    fetchOptions();
+  }, []);
 
   useEffect(() => {
     axios
@@ -58,8 +110,94 @@ function FichaJogo() {
   };
 
   const fichaEquipa = async (values) => {
-    // Implement your logic for fichaEquipa here
+    try {
+      console.log('Form Values:', values);
+
+      // Clear previous data and set visibility
+      setFormSubmitted(false);
+      setDetailedJogadores([]);
+      setDetailedTecnico([]);
+
+      const response = await fetch(`http://localhost:3001/equipa/check?ClubeId=${values.ClubeId}&EscalaoId=${values.EscalaoId}&CurrentYear=${new Date().getFullYear()}`);
+      const equipaIdData = await response.json();
+
+      if (equipaIdData.equipaId != null) {
+        console.log('Combination of ClubeId and EscalaoId exists in equipas table for the current year.');
+        console.log("equipaId value:", equipaIdData.equipaId);
+
+
+        console.log("Jogador")
+
+        // Fetch jogadores data from the server for the given equipaId
+        const jogadoresResponse = await fetch(`http://localhost:3001/equipa_jogadores/check?equipaId=${equipaIdData.equipaId}`);
+        const jogadoresData = await jogadoresResponse.json();
+
+        if (jogadoresData.length > 0) {
+          console.log('Jogadores found in equipaJogadores table for equipaId:', equipaIdData.equipaId);
+          console.log('Jogadores:', jogadoresData);
+
+          // Fetch detailed information for each JogadoreId in the array
+          const detailedJogadores = await Promise.all(
+            jogadoresData.map(async (jogadorId) => {
+              const jogadorResponse = await fetch(`http://localhost:3001/jogadores/${jogadorId}`);
+              const jogadorInfo = await jogadorResponse.json();
+              return jogadorInfo;
+            })
+          );
+
+          setDetailedJogadores(detailedJogadores);
+
+          console.log('Detailed Jogadores:', detailedJogadores);
+
+
+        } else {
+          console.log('No jogadores found in equipaJogadores table for equipaId:', equipaIdData.equipaId);
+
+        }
+        console.log("Tecnico")
+        // Fetch tecnicos data from the server for the given equipaId
+        const tecnicosResponse = await fetch(`http://localhost:3001/equipa_tecnica/check?equipaId=${equipaIdData.equipaId}`);
+        const tecnicosData = await tecnicosResponse.json();
+
+        if (tecnicosData.length > 0) {
+          console.log('Tecnicos found in equipaTecnicas table for equipaId:', equipaIdData.equipaId);
+          console.log('Tecnicos:', tecnicosData);
+
+          const detailedTecnico = await Promise.all(
+            tecnicosData.map(async (tecnicosIds) => {
+              const tecnicoResponse = await fetch(`http://localhost:3001/tecnicos/${tecnicosIds}`);
+              const tecnicoInfo = await tecnicoResponse.json();
+              return tecnicoInfo;
+            })
+          );
+
+          setDetailedTecnico(detailedTecnico);
+
+          console.log('Detailed Tecnicos:', detailedTecnico);
+
+
+        } else {
+          console.log('No tecnicos found in tecnicosEquipa table for equipaId:', equipaIdData.equipaId);
+
+        }
+
+
+      } else {
+        // Show a popup message indicating that the combination is not found
+        setShowAlert(true);
+      }
+      setFormSubmitted(true);
+
+    } catch (error) {
+      // Handle error
+      console.error('Error:', error);
+    }
   };
+
+  const handleCloseAlert = () => {
+    setShowAlert(false);
+  };
+
 
   return (
     <div className='FichaJogo'>
@@ -93,7 +231,7 @@ function FichaJogo() {
                     </option>
                   ))}
                 </Field>
-                <ErrorMessage name='Home' component='div' className='error' />
+                <ErrorMessage name='Home' component='span' />
 
                 <label className='field'>Equipa 2:</label>
                 <Field as='select' id='select2' name='Away'>
@@ -104,7 +242,7 @@ function FichaJogo() {
                     </option>
                   ))}
                 </Field>
-                <ErrorMessage name='Away' component='div' className='error' />
+                <ErrorMessage name='Away' component='span' />
                 <div className='btnInfo'>
                   <button type='submit'>Obter Ficha Jogo</button>
                 </div>
@@ -117,7 +255,7 @@ function FichaJogo() {
       </div>
 
       <div className='formRight2'>
-        <Formik initialValues={initialValues} onSubmit={fichaEquipa} validationSchema={validationSchema}>
+        <Formik initialValues={initialValues1} onSubmit={fichaEquipa} validationSchema={validationSchema1}>
           <Form className='formContainer' encType="multipart/form-data">
             <div style={{ textAlign: 'center' }}>
               <h1>Ficha Equipa</h1>
@@ -157,6 +295,94 @@ function FichaJogo() {
           </Form>
         </Formik>
       </div>
+      <div className='bellow'>
+        {formSubmitted && (
+          <>
+            <div className='header-info'>
+              <h2>Época: {new Date().getFullYear()}</h2>
+              <h2>Equipa: {detailedJogadores[0].Clube || ""}</h2>
+              <h2>Escalão: {escalaoOptions2.find((escalao) => escalao.id === detailedJogadores[0].EscalaoId)?.Nome || ""}</h2>
+            </div>
+            {detailedJogadores.length > 0 && (
+
+              <div className="info-table2">
+                <table>
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>Nome</th>
+                      <th>Data Nascimento</th>
+                      <th>CC Jogador</th>
+                      <th>Residente</th>
+                      {/* Add more columns based on the detailed jogadores information */}
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {detailedJogadores.map((item) => (
+                      <tr key={item.id}>
+                        <td>{item.id}</td>
+                        <td>{item.Nome}</td>
+                        <td>{item.DataNascimento}</td>
+                        <td>{item.CCJogador.substring(0, 8)}</td>{/*.substring(0, 8) show only the first 8 digits*/}
+                        <td
+                          style={{
+                            color: item.Reside ? 'inherit' : 'inherit',
+                            backgroundColor: item.Reside ? 'inherit' : '#A45A52'
+                          }}
+                        >
+                          {item.Reside ? 'Sim' : 'Não'}
+                        </td>
+
+                        {/* Add more cells based on the detailed jogadores information */}
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+
+            )}
+            {detailedTecnico.length > 0 && (
+              <div className="info-table2">
+                <table>
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>Nome</th>
+                      <th>Data Nascimento</th>
+                      <th>CC Tecnico</th>
+                      <th>Tipo Tecnico</th>
+                      {/* Add more columns based on the detailed tecnico information */}
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {detailedTecnico.map((item) => (
+                      <tr key={item.id}>
+                        <td>{item.id}</td>
+                        <td>{item.Nome}</td>
+                        <td>{item.DataNascimento}</td>
+                        <td>{item.CCTecnico.substring(0, 8)}</td>
+                        <td>
+                          {tecnicosType.find((type) => type.id === item.TecnicosTypeId)?.Nome || 'Unknown'}
+                        </td>
+                        {/* Add more cells based on the detailed tecnico information */}
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            )}
+          </>
+        )}
+      </div>
+
+      {showAlert && (
+        <div className='custom-alert-overlay2'>
+          <div className="custom-alert2">
+            <p>Clube não inscrito no escalão selecionado!</p>
+            <button onClick={handleCloseAlert}>OK</button>
+          </div>
+        </div>
+      )}
     </div >
 
   )
